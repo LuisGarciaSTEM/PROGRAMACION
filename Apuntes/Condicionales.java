e1e2ced54d5edd0ef8d0f0aeaaa3886853111313
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-08 10:42:23 +0100

    Cambios en condicionales

--- Apuntes/Condicionales.java
+++ Apuntes/Condicionales.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Condicionales {
     public static void main(String[] args) {
         int numero = 3;
+        String palabra;
+        Scanner sc = new Scanner(System.in);
 
         if (numero < 5) {
             System.out.println("El número es menor que 5");
@@ -13,15 +15,23 @@ public class Condicionales {
         }
 
         /* Solicita un número por teclado, y di si es positivo o negativo. */
-        Scanner sc = new Scanner(System.in);
+
         System.out.println("Dime un número, para saber si es positivo o negativo.");
         int pedirNumero = sc.nextInt();
-        
 
         if (pedirNumero < 0) {
-            System.out.println("El número es negativo");
+            System.out.println("El número es: " + pedirNumero + " y es negativo");
         } else {
-            System.out.println("El número es positivo");
+            System.out.println("El número es: " + pedirNumero + " y es positivo");
+        }
+
+        sc.nextLine(); // Coge el salto de linea que ha dejado nextInt
+        System.out.println("Dime un palabra, y la repito.");
+        palabra = sc.nextLine();
+        if (palabra == "caca") {
+            System.out.println("La palabra " + palabra + " = a");
+        } else {
+            System.out.println("La palabra " + palabra + " != a");
         }
 
     }
