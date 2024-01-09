01755ec83cc0bdb8f90e99c97f1140c1153960c8
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-09 12:37:19 +0100

    Ejercicio 1

---
+++ Ejercicios/Ejercicios.java
@@ -0,0 +1,42 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicios {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pedirNum;
+        /*
+         * 1.- Condición básica.
+         * Crea una variable y dale un valor.
+         * Si dicho valor es menor que 5, que escriba por pantalla 'es menor que 5'. Si
+         * es mayor que 5, que escriba por pantalla 'es mayor que 5. Si es igual a 5,
+         * que escriba por pantalla 'es 5'.
+         * 
+         * 
+         * int num1 = 5;
+         * 
+         * System.out.println("Dime un número");
+         * pedirNum = sc.nextInt();
+         * 
+         * if (pedirNum < num1){
+         * System.out.println("Es menor que 5.");
+         * } else if (pedirNum > num1) {
+         * System.out.println("Es mayor que 5.");
+         * } else {
+         * System.out.println("Son iguales");
+         * }
+         */
+
+        /*
+         * 2.- Variable introducida por teclado.
+         * Solicita un valor para una variable numérica. Si dicho valor es menor que 0,
+         * que escriba por pantalla 'es un número negativo'. Si dicho valor es mayor o
+         * igual que 0, que escriba por pantalla 'es un número positivo'.
+         */
+            
+        pedirNum = sc.nextInt();
+        if (pedirNum < )
+        
+    }
+}
