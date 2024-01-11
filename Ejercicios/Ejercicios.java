b2648130313e5f8ea8911b7aa6b83db58c7a0774
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-11 18:48:12 +0100

    Ejercicios 3 y 4

--- Ejercicios/Ejercicios.java
+++ Ejercicios/Ejercicios.java
@@ -6,6 +6,7 @@ public class Ejercicios {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int pedirNum;
+        int pedirNum2;
         /*
          * 1.- Condición básica.
          * Crea una variable y dale un valor.
@@ -33,10 +34,72 @@ public class Ejercicios {
          * Solicita un valor para una variable numérica. Si dicho valor es menor que 0,
          * que escriba por pantalla 'es un número negativo'. Si dicho valor es mayor o
          * igual que 0, que escriba por pantalla 'es un número positivo'.
+         * 
+         * System.out.println("Dime un número para saber si es positivo o negativo: ");
+         * pedirNum = sc.nextInt();
+         * if (pedirNum < 0) System.out.println("El número es negativo.");
+         * else System.out.println("El número es positivo.");
          */
-            
-        pedirNum = sc.nextInt();
-        if (pedirNum < )
-        
+
+        /*
+         * 3.- Argumentos introducidos al ejecutar.
+         * A la hora de ejecutar el código, añade un número que pasará como argumento
+         * del main.
+         * Gestiona que realemente introduce un único argumento, ni más ni menos.
+         * Si dicho valor es menor que 0, que escriba por pantalla 'es un número
+         * negativo'.
+         * Si dicho valor es mayor o igual que 0, que escriba por pantalla 'es un número
+         * positivo'.
+         * Gestiona para que, si el número de argumentos es incorrectos, los solicite
+         * desde teclado.
+         * 
+         * if (args.length == 1) {
+         * int numero = Integer.parseInt(args[0]);
+         * if (numero >= 0)
+         * System.out.println("El número es positivo");
+         * else
+         * System.out.println("El número es negativo");
+         * } else {
+         * System.out.println("Dime un número");
+         * pedirNum = sc.nextInt();
+         * if (pedirNum < 0)
+         * System.out.println("El número es negativo.");
+         * else
+         * System.out.println("El número es positivo.");
+         * }
+         */
+
+        /*
+         * 4.- Varios argumentos introducidos al ejecutar.
+         * A la hora de ejecutar el código, añade dos números que pasarán como argumento
+         * del main.
+         * Gestiona que realemente introduce dos argumentos, ni más ni menos.
+         * Hay que imprimir por pantalla el número que sea mayor. Gestiona para que, si
+         * el número de argumentos es incorrectos, los solicite desde teclado.
+         */
+
+        if (args.length == 2) {
+            int num1 = Integer.parseInt(args[0]);
+            int num2 = Integer.parseInt(args[1]);
+
+            if (num1 > num2)
+                System.out.println("El número " + num1 + " es mayor que " + num2);
+            else if (num1 < num2)
+                System.out.println("El número " + num2 + " es mayor que " + num1);
+            else
+                System.out.println("Los números son iguales. Son el: " + num1);
+        } else {
+            System.out.println("Introduce 2 números separados por un espacio para saber cuál es el mayor: ");
+            pedirNum = sc.nextInt();
+            pedirNum2 = sc.nextInt();
+
+            if (pedirNum > pedirNum2)
+                System.out.println("El número " + pedirNum + " es mayor que " + pedirNum2);
+            else if (pedirNum < pedirNum2)
+                System.out.println("El número " + pedirNum2 + " es mayor que " + pedirNum);
+            else
+                System.out.println("Los números son iguales. Son el: " + pedirNum);
+        }
     }
+
 }
