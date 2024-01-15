d848442d20a527756a25e1e4e4531468cc84ee90
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-15 11:24:40 +0100

    Apuntes bucles

---
+++ Apuntes/Bucles.java
@@ -0,0 +1,19 @@
+public class Bucles {
+    public static void main(String[] args) {
+        
+        // For each -> for color in listaColores
+
+        String[] listaAzules = {"Azul", "Añil", "Indigo", "Cobalto", "Klein"};
+        for(String azul : listaAzules){
+            System.out.println(azul);
+
+        }
+
+        // For -> NO existe en Python...
+        // for (incializmos variables; condicion while; de cuánto en cuánto van aumentando)
+        for(int contador = 0; contador < 50; contador ++){
+
+        }
+
+    }
+}
