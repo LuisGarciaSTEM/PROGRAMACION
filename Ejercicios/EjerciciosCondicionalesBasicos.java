33f9b58ad9421eb2fddc8918905a249f984cc376
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-15 12:31:17 +0100

    Par: Solicita un número par, si el número introducido no es un número par lanza un mensaje de error.

--- Ejercicios/EjerciciosCondicionalesBasicos.java
+++ Ejercicios/EjerciciosCondicionalesBasicos.java
@@ -11,16 +11,53 @@ public class EjerciciosCondicionalesBasicos {
         /*
          * Numero natural: Solicita un número, si es menor que 0 imprime que no es un
          * número natural.
+         *
+         * System.out.println("Dime un número para saber si es natural: ");
+         * numero = sc.nextInt();
+         * if (numero < min) {
+         * System.out.println("No es un número natural.");
+         * } else if (numero > min) {
+         * System.out.println("Sí es un número natural.");
+         * } else {
+         * System.out.println("Has introducido algo erroneo.");
+         * }
+         * 
          */
-        
-        System.out.println("Dime un número para saber si es natural: ");
-        numero = sc.nextInt();
-        if (numero < min) {
-            System.out.println("No es un número natural.");
-        } else if (numero > min) {
-            System.out.println("Sí es un número natural.");
-        } else {
-            System.out.println("Has introducido algo erroneo.");
-        }
+
+        /*
+         * - Hora: Solicita una hora (hora,minutos,segundos) en una única línea,
+         * separada por espacios.
+         * Si los datos son incorrectos, lanza un mensaje de error.
+         * 
+         * System.out.
+         * print("Ingrese la hora (hora minutos segundos separados por espacios): ");
+         * int hora = sc.nextInt();
+         * int minutos = sc.nextInt();
+         * int segundos = sc.nextInt();
+         * if ((hora >= 0 && hora < 24) && (minutos >= 0 && minutos < 60) && (segundos
+         * >= 0 && segundos < 60)) {
+         * System.out.println("Hora ingresada: " + hora + ":" + minutos + ":" +
+         * segundos);
+         * } else {
+         * System.out.println("Error: La hora ingresada es inválida.");
+         * }
+         */
+
+        /*
+         * Par: Solicita un número par, si el número introducido no es un número par
+         * lanza un mensaje de error.
+         * 
+         * 
+         * System.out.println("Dime un número para saber si es par o no.");
+         * numero = sc.nextInt();
+         * 
+         * if (numero % 2 == 0) {
+         * System.out.println("El número es par!");
+         * } else {
+         * System.out.println("Es impar!");
+         * }
+         */
+
+         
     }
 }
