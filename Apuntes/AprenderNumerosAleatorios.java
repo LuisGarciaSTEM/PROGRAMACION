a8411a80a4900d042fa5f392fab8bf19be4ebc8c
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-16 11:07:20 +0100

    Números básicos del 1 al 5

--- Apuntes/AprenderNumerosAleatorios.java
+++ Apuntes/AprenderNumerosAleatorios.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+
 public class AprenderNumerosAleatorios {
-    public class main(String[] args){
-        
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int numero;
+
+        numero = rand.nextInt();
+        System.out.println("Todos los números: " + numero);
+
+        // Número entre 0 y 4
+        numero = rand.nextInt(5);
+        System.out.println("Todos números hasta 5: " + numero);
+
+        // Número entre 0 y 5
+        numero = rand.nextInt(5 + 1);
+        System.out.println("Todos números hasta 5: " + numero);
+
+        // Número entre 1 y 5
+        numero = rand.nextInt(5) + 1;
+        System.out.println("Todos números hasta 5: " + numero);
     }
 }
