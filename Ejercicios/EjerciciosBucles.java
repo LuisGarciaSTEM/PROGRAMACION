e9dc61a4498d4c7401f3f1ce926da94fabbab156
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-16 10:18:35 +0100

    Ejer bucles

--- Ejercicios/EjerciciosBucles.java
+++ Ejercicios/EjerciciosBucles.java
@@ -1,10 +1,23 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class EjerciciosBucles {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int numero = 1;
+        char letra;
+        
         // Numero natural: Solicita un número hasta que te introduzcan un número natural (positivo).
+       
+        do {
+            System.out.println("Dime un número natural positivo!!! : ");
+            numero = sc.nextInt();
+        } while (numero < 0);
+        
+        // Solicita una letra 
 
+        System.out.println("Introduce una letra: ");
 
     }
 }
