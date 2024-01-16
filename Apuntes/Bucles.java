8b114cd531b997b10dccfb528d19d86875f23604
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-16 08:43:24 +0100

    doWhile y while

--- Apuntes/Bucles.java
+++ Apuntes/Bucles.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class Bucles {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String palabra;
+        int numero;
+        boolean salir = true;
 
         // For each -> for color in listaColores
-
         String[] listaAzules = { "Azul", "Añil", "Indigo", "Cobalto", "Klein" };
         for (String azul : listaAzules) {
             System.out.println(azul);
@@ -21,10 +26,30 @@ public class Bucles {
             for (int j = 0; j < 10; j++) {
                 System.out.print("" + i + j + " ");
             }
-            System.out.println();
+
         }
 
-        
+        System.out.println("Bucles doWhile");
+        // do {...} while()
+        do {
+            System.out.println("Escribe salir: ");
+            palabra = sc.nextLine();
+        } while (!palabra.equals("salir"));
+
+        do {
+            System.out.println("Introduce un número entre 1 y 50: ");
+            numero = sc.nextInt();
+        } while (numero < 1 || numero > 50);
+
+
+        System.out.println("Bucle while");
+        while (!salir) {
+            System.out.println("Escribe salir: ");
+            palabra = sc.nextLine();
+            if (palabra.equals("salir")) {
+                salir = true;
+            }
+        }
 
     }
 }
