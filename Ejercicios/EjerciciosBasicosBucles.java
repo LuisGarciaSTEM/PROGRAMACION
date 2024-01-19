5cc2ef4384cf1cfecf20344b416ebedbd64a37ac
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-19 09:25:05 +0100

    Numero natural: Solicita un número hasta que te introduzcan un número natural (positivo)

--- Ejercicios/EjerciciosBasicosBucles.java
+++ Ejercicios/EjerciciosBasicosBucles.java
@@ -1,18 +1,25 @@
 package Ejercicios;
 
+import java.util.Scanner;
 
 public class EjerciciosBasicosBucles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num;
         
         
         // Numero natural: Solicita un número hasta que te introduzcan un número natural
         // (positivo)
 
         do{
+            System.out.println("Dime un número natural positivo: ");
+            num = sc.nextInt(); 
+        }while (num<0);
 
-        }while {
+        System.out.println("Corrrecto, " + num + " es un número positivo.");
 
-        }
+        
+
+        
     }
 }
