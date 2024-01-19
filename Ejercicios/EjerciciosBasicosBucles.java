35e0631e02000166e2354bfa5724c97928510e87
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-19 09:26:03 +0100

    Retoque

--- Ejercicios/EjerciciosBasicosBucles.java
+++ Ejercicios/EjerciciosBasicosBucles.java
@@ -6,20 +6,16 @@ public class EjerciciosBasicosBucles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num;
-        
-        
+
         // Numero natural: Solicita un número hasta que te introduzcan un número natural
         // (positivo)
 
-        do{
-            System.out.println("Dime un número natural positivo: ");
-            num = sc.nextInt(); 
-        }while (num<0);
+        do {
+            System.out.println("Dime un número natural positivo (mayor que 0): ");
+            num = sc.nextInt();
+        } while (num <= 0);
 
         System.out.println("Corrrecto, " + num + " es un número positivo.");
 
-        
-
-        
     }
 }
