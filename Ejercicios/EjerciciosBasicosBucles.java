2bfd27a9120f8c8e92e2147216ee34a535224d89
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-19 09:21:49 +0100

    Ejrcicios basicos bucles

---
+++ Ejercicios/EjerciciosBasicosBucles.java
@@ -0,0 +1,18 @@
+package Ejercicios;
+
+
+public class EjerciciosBasicosBucles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        // Numero natural: Solicita un número hasta que te introduzcan un número natural
+        // (positivo)
+
+        do{
+
+        }while {
+
+        }
+    }
+}
