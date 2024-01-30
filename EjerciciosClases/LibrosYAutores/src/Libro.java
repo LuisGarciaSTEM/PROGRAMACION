c002bb9588022bf2661a09cf7fa594e6621f91f5
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-01-30 11:02:48 +0100

    cchamge

--- EjerciciosClases/LibrosYAutores/src/Libro.java
+++ EjerciciosClases/LibrosYAutores/src/Libro.java
@@ -1,11 +1,14 @@
 public class Libro {
     
-    private String nombreAutor;
+    private String titulo;
     private String autor;
     private final int ISBN; // Una vez que se le da un valor no puede cambiar (final) 
     private int numPagina;
     
-    
+    public Libro(String titulo, int ISBN){
+        this.titulo = titulo;
+        this.ISBN = ISBN;
+    }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
