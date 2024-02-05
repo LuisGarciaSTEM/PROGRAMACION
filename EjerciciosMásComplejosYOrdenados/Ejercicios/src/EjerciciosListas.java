c8f00ad12756ba74846363c0a592f9fecc629717
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-02-05 11:53:18 +0100

    Lista en código Crea una lista de 5 palabras directamente en el código.

--- EjerciciosMásComplejosYOrdenados/Ejercicios/src/EjerciciosListas.java
+++ EjerciciosMásComplejosYOrdenados/Ejercicios/src/EjerciciosListas.java
@@ -1,7 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public interface EjerciciosListas {
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        Scanner sc = new Scanner(System.in);
+        // Lista en código Crea una lista de 5 palabras directamente en el código.
+        ArrayList<String> lista5Palabras = new ArrayList<String>();
 
-        
+        lista5Palabras.add("Palabra 1");
+        lista5Palabras.add("Palabra 2");
+        lista5Palabras.add("Palabra 3");
+        lista5Palabras.add("Palabra 4");
+        lista5Palabras.add("Palabra 5");
+
+        System.out.println(lista5Palabras);
+
+
+
     }
 }
