5ef3af1b20e95a38a84e78f8eaef3cdc88e900ec
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-02-05 12:50:50 +0100

    Lista con Scanner Crea una lista de 5 palabras, solicitándolas por consola.

--- EjerciciosMásComplejosYOrdenados/Ejercicios/src/EjerciciosListas.java
+++ EjerciciosMásComplejosYOrdenados/Ejercicios/src/EjerciciosListas.java
@@ -5,17 +5,30 @@ public interface EjerciciosListas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // Lista en código Crea una lista de 5 palabras directamente en el código.
-        ArrayList<String> lista5Palabras = new ArrayList<String>();
+        /*
+         * ArrayList<String> lista5Palabras = new ArrayList<String>();
+         * 
+         * lista5Palabras.add("Palabra 1");
+         * lista5Palabras.add("Palabra 2");
+         * lista5Palabras.add("Palabra 3");
+         * lista5Palabras.add("Palabra 4");
+         * lista5Palabras.add("Palabra 5");
+         * 
+         * System.out.println(lista5Palabras);
+         */
 
-        lista5Palabras.add("Palabra 1");
-        lista5Palabras.add("Palabra 2");
-        lista5Palabras.add("Palabra 3");
-        lista5Palabras.add("Palabra 4");
-        lista5Palabras.add("Palabra 5");
-
-        System.out.println(lista5Palabras);
-
-
-
+        // Lista con Scanner Crea una lista de 5 palabras, solicitándolas por consola.
+        /*
+         * ArrayList<String> lista5PalabrasPorConsola = new ArrayList<>();
+         * System.out.println("Dime 5 palabras para añadir a la lista: ");
+         * 
+         * for (int i = 1; i < 6; i++) {
+         * System.out.print(i + ": ");
+         * String pedirPalabras = sc.nextLine();
+         * lista5PalabrasPorConsola.add(pedirPalabras);
+         * }
+         * 
+         * System.out.println(lista5PalabrasPorConsola);
+         */
     }
 }
