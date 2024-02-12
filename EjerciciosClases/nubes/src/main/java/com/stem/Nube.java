4d0ec5dd8928c5ce04166afbb6dec0339cdd8e20
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-02-12 11:50:13 +0100

    hacer unirNube

--- EjerciciosClases/nubes/src/main/java/com/stem/Nube.java
+++ EjerciciosClases/nubes/src/main/java/com/stem/Nube.java
@@ -1,13 +1,19 @@
 package com.stem;
 
+import java.util.Scanner;
+
 public class Nube {
 
     // Clase chiquitas que se usan dentro de nube.
     public enum TiposDeNubes {
         CIRROS,
-        CUMULAS,
+        CUMULOS,
         ESTRATOS,
-        NIMBOS
+        NIMBOS;
+
+        public static String mostrar() {
+            return " [CIRROS, CUMULOS, ESTRATOS, NIMBOS]";
+        }
     }
 
     private TiposDeNubes tipo;
@@ -48,7 +54,40 @@ public class Nube {
         this.tamanio = tamanio;
     }
 
+    public static void unirNube(Nube nube1, Nube nube2) {
+        // Una nube que llega nueva une a this.nube si estan a la misma altura.
+        if (nube1.altura == nube2.altura) {
+            nube1.tamanio += nube2.tamanio;
+            nube2.tamanio = 0;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Altura: " + this.altura + " tamaño: " + this.tamanio;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Hay que poner TiposDeNube.(lo que sea de las 4 opciones).
+        Nube nube1 = new Nube(TiposDeNubes.NIMBOS, 10, 30);
+        // Nube nube2 = new Nube(NIMBOS, 10, 30) --> ERROR ;
+        // Nube nube3 = new Nube("TiposDeNubes.CUMULAS", 10, 30)--> ERROR ;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Intoduce el tipo de nube [" + TiposDeNubes.mostrar() + "]");
+
+        String tipoString = sc.nextLine().toUpperCase();
+
+        TiposDeNubes tipoEmun = TiposDeNubes.valueOf(tipoString);
+
+        Nube nube2 = new Nube(tipoEmun, 10, 30);
+
+        System.out.println(nube1);
+        System.out.println(nube2);
+        unirNube(nube1, nube2);
+        System.out.println(nube1);
+        System.out.println(nube2);
+
     }
 }
