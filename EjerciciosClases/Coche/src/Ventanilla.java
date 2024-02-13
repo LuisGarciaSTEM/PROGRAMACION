8900436eb6fd2094af2ccd6b799f58675b41b6f8
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-02-13 11:25:39 +0100

    los get y los setter

--- EjerciciosClases/Coche/src/Ventanilla.java
+++ EjerciciosClases/Coche/src/Ventanilla.java
@@ -1,38 +1,54 @@
 public class Ventanilla {
     private boolean transparencia; // Lo que no es transparente son tintados.
-    private double tamanio; 
+    private double tamanio;
     private boolean estado; // Subida o bajada.
 
-    public Ventanilla(){
+    public Ventanilla() {
         this.transparencia = transparencia;
         this.transparencia = true;
         this.tamanio = tamanio;
-        this.tamanio = 15; 
-        this.estado = estado; 
-        this.estado = true;  
+        this.tamanio = 15;
+        this.estado = estado;
+        this.estado = true;
     }
 
-    public Ventanilla(boolean transparencia, double tamanio){
-        this.transparencia = transparencia; 
-        this.tamanio = tamanio; 
-        this.estado = true;   
+    public Ventanilla(boolean transparencia, double tamanio) {
+        this.transparencia = transparencia;
+        this.tamanio = tamanio;
+        this.estado = true;
     }
 
-    public Ventanilla(Ventanilla v){
+    public Ventanilla(Ventanilla v) {
         this.transparencia = v.transparencia;
-        this.tamanio = v.tamanio; 
-        this.estado = v.estado; 
+        this.tamanio = v.tamanio;
+        this.estado = v.estado;
     }
 
+    public boolean getTransparencia() {
+        return transparencia;
+    }
 
     public double getTamanio() {
         return tamanio;
     }
-    
 
+    public boolean getEstado() {
+        return transparencia;
+    }
 
+    public void setTamanio(double tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public void setTransparencia(boolean transparencia) {
+        this.transparencia = transparencia;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 
     public static void main(String[] args) {
-        
+
     }
 }
