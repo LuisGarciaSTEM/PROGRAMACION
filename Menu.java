557332ef5119914472221d5985f734ba9b028473
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-04-16 10:49:21 +0200

    ajbdshba

--- Menu.java
+++ Menu.java
@@ -1,3 +1,13 @@
 public class Menu {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce que operacion quieres realizar:");
+        System.out.println("1. Sumar.");
+        System.out.println("2. Restar.");
+        System.out.println("3. Multiplicar.");
+        System.out.println("4. Dividir.");
+        System.out.println("5. Salir.");
+
+    }
 }
