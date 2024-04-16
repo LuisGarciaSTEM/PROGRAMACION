7622676c4d73c3c6079f54c9c0932c315c30e90d
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-04-16 10:54:03 +0200

    Meter en los ejercicios

--- EjerciciosBasicos/Menu.java
+++ EjerciciosBasicos/Menu.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Menu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int numOperacion = 0;
 
         System.out.println("Introduce que operacion quieres realizar:");
         System.out.println("1. Sumar.");
@@ -8,6 +11,17 @@ public class Menu {
         System.out.println("3. Multiplicar.");
         System.out.println("4. Dividir.");
         System.out.println("5. Salir.");
+        numOperacion = Integer.parseInt(sc.nextLine());
+
+        switch (sc) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+        
 
     }
 }
