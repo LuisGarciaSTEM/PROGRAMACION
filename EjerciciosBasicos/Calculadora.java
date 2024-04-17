31589a157be3ce23401662615236e1fdf87922c0
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-04-17 21:00:05 +0200

    Cambio de los nombres los tenía al reves.

--- EjerciciosBasicos/Calculadora.java
+++ EjerciciosBasicos/Calculadora.java
@@ -2,59 +2,98 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-    private static double saldo = 1000.0;
+    static Scanner sc = new Scanner(System.in);
 
-    private static void imprimirMenu() {
-        System.out.println("1. Ingresar dinero.");
-        System.out.println("2. Retirar dinero.");
-        System.out.println("3. Mostrar dinero actual.");
-        System.out.println("4. Salir.");
+    private static String input() {
+        return sc.nextLine();
+    }
+
+    private static int pedirNum() {
+        while (true) {
+            try {
+                System.out.println("\nDime el número de la operación.");
+                int num = Integer.parseInt(input());
+                return num;
+            } catch (NumberFormatException e) {
+                System.out.println("\nERROR: Has introducido un número mal. Inténtalo de nuevo.");
+            }
+        }
+    }
+
+    private static int devolverNumMenu() {
+        while (true) {
+            try {
+                System.out.println("\nIntroduce qué operación quieres realizar:");
+                System.out.println("1. Sumar.");
+                System.out.println("2. Restar.");
+                System.out.println("3. Multiplicar.");
+                System.out.println("4. Dividir.");
+                System.out.println("5. Salir.");
+                return Integer.parseInt(input());
+            } catch (Exception e) {
+                System.out.println("\nERROR: Has introducido un número mal. Inténtalo de nuevo.");
+            }
+        }
+
+    }
+
+    private static int suma(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    private static int resta(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    private static int multiplicacion(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    private static int division(int num1, int num2) {
+        int devolver;
+        try {
+            devolver = num1 / num2;
+        } catch (Exception e) {
+            System.out.println("\nERROR: No se puede dividir por cero. Inténtalo de nuevo.");
+            devolver = -1;
+        }
+        return devolver;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Tu saldo es: " + saldo);
-        boolean salir = false;
-        double dineroIngresado;
+        boolean continuar = true;
 
-        while (!salir) {
-            imprimirMenu();
-            int opcion = 0;
-            try {
-                System.out.println("¿Qué quieres hacer con tu dinero: ?");
-                opcion = sc.nextInt();
-            } catch (Exception e) {
-                System.out.println("Error: Debes introducir un número.");
-                sc.nextLine(); // Limpiar el buffer de entrada
+        while (continuar) {
+            switch (devolverNumMenu()) {
+                case 1 -> {
+                    System.out.println("\nResultado de la suma: " 
+                    + suma(pedirNum(), pedirNum()));
+                }
+
+                case 2 -> {
+                    System.out.println("\nResultado de la resta: " 
+                    + resta(pedirNum(), pedirNum()));
+                }
+
+                case 3 -> {
+                    System.out.println("\nResultado de la multiplicación: " 
+                    + multiplicacion(pedirNum(), pedirNum()));
+                }
+
+                case 4 -> {
+                    System.out.println("\nResultado de la división: " 
+                    + division(pedirNum(), pedirNum()));
+                }
+
+                case 5 -> {
+                    continuar = false;
+                    System.out.println("\nSaliendo del programa...");
+                }
+
+                default -> {
+                    System.out.println("\nOpción inválida, intenta nuevamente.");
+                }
             }
-
-            switch (opcion) {
-                case 1:
-                    System.out.println("¿Cuánto dinero deseas ingresar?");
-                    dineroIngresado = sc.nextDouble();
-                    saldo += dineroIngresado;
-                    break;
-                case 2:
-                    System.out.println("¿Cuánto dinero deseas retirar?");
-                    double dineroRetirar = sc.nextDouble();
-                    if (dineroRetirar > saldo) {
-                        System.out.println("Saldo insuficiente.");
-                    } else {
-                        saldo -= dineroRetirar;
-                    }
-                    break;
-                case 3:
-                    System.out.println("Tu saldo actual es: " + saldo);
-                    break;
-                case 4:
-                    System.out.println("Saliendo del programa.");
-                    salir = true;
-                    break;
-                default:
-                    System.out.println("Has introducido una opción inválida.");
-            }
-
         }
     }
 }
-
